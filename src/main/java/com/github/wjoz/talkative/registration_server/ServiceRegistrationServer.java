9c4c56deac844f2c20aa816ad8ea5894b33c5816
Woj Joz <wjozefek@gmail.com>
2017-02-22 00:39:24 +0100

    Renamed the YAML file and configuration

--- src/main/java/com/github/wjoz/talkative/registration_server/ServiceRegistrationServer.java
+++ src/main/java/com/github/wjoz/talkative/registration_server/ServiceRegistrationServer.java
@@ -8,7 +8,6 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 public class ServiceRegistrationServer {
     public static void main( String[] args ) {
-        System.setProperty("spring.config.name", "registration-server");
         SpringApplication.run(ServiceRegistrationServer.class, args);
     }
 }
